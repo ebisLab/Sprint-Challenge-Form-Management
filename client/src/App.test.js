ecdcb47ecd22e0ddd1988dfc9b6d025d51fce4fb
ebisLab <ebaiden@techie.com>
2019-08-09 15:50:01 -0400

    tried testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import UserForm from './Components/Form'
 import {render, fireEvent} from '@testing-library/react';
-// import '@testing-library/react/cleanup-after-each'; 
+import '@testing-library/react/cleanup-after-each'; 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('<App>', () => {
+  it('renders without crashing', () => {
+      render (<App />);
+  });
+  it('renders login button', () => {
+    const userform = render (<UserForm />);
+
+    userform.getByText(/login/i)
 });
+})
