54dce627c5db458157f4909153e4c92652548ffc
ebisLab <ebaiden@techie.com>
2019-08-09 12:43:14 -0400

    the added formiklogin logic

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {Form, Field, withFormik, Formik, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
 
 
-function UserForm(props){
-    
+function UserForm(){
+
+
     return (
         <div>
             <h1>Form Section</h1>
            
-            <form>
+            <Form>
             <input
             type="text"
             name="username"
-            onChange={``}
             placeholder="username"
           />
           <input
             type="text"
             name="password"
-            onChange={``}
             placeholder="password"
           />
-            </form>
-            <button>Login</button>
+          <button>Login</button>
+            </Form>
+            
             
             </div>
     );
 };
+
+
+const FormikLoginForm = withFormik({
+    mapPropsToValues({name, password}){
+        return{
+            name: name || '', 
+            password: password || '',
+        }
+    }
+})(UserForm)
 
 export default UserForm;
