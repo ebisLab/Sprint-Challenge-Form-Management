72c9a53cd0d17f8f76d2efc672a29da83d8f00c1
ebisLab <ebaiden@techie.com>
2019-08-09 14:14:03 -0400

    axios request

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UserForm from './Components/Form'
+import axios from 'axios';
 
 class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      data: ''
+      data: []
     }
+
+  
   }
+
 
   render(){
     return (
--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -4,7 +4,27 @@ import {Form, Field, withFormik} from 'formik';
 import * as Yup from 'yup';
 
 
-function UserForm({ errors,values,status }){
+function UserForm({ errors, touched, values, status }){
+    const[users, setUsers] = useState([]);
+    const[dataInfo, setDataInfo] = useState([]);
+
+    useEffect(() => {
+    //    const getData = () => {
+    //   axios
+    //     .get('http://localhost:5000/api/restricted/data')
+    //     .then(response => {
+    //       console.log(response.data)
+    //       setDataInfo(response.data);
+    //     })
+    //     .catch(error => {
+    //       console.error('Server Error', error);
+    //     });
+    // }
+    
+    // getData();
+   
+    })
+console.log(errors, 'errors')
 
 
     return (
@@ -12,13 +32,14 @@ function UserForm({ errors,values,status }){
             <h1>Form Section</h1>
            
             <Form>
-            {errors.username && <p>{errors.username}</p>}
-
+            {touched.username && errors.username && <p>{errors.username}</p>}
             <Field className="user-input"
             type="text"
             name="username"
             placeholder="username"
           />
+            {touched.password && errors.password && <p>{errors.password}</p>}
+
           <Field
             type="text"
             name="password"
@@ -51,6 +72,18 @@ const FormikLoginForm = withFormik({
     //get setStatus
     handleSubmit(values){
     console.log(values)
+
+    //form submission HTTP request
+    axios
+        .post(`http://localhost:5000/api/register`, values)
+        .then(res=> {
+            // console.log('Set Status Area')
+            console.log('res', res.data)
+            // resetForm();
+            // setStatus(res.data)
+        })
+        
+        .catch(err => console.log(err) )
     }
 
 })(UserForm)
